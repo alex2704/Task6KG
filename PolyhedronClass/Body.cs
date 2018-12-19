d298e8d68e961aec77adb11066ce8e88679ba2a2
alex2704 <34662549+alex2704@users.noreply.github.com>
2018-12-19 06:41:48 +0300

    Созданы матрицы многогранников, также поля для тела многогранника и конструктор

--- PolyhedronClass/Body.cs
+++ PolyhedronClass/Body.cs
@@ -8,5 +8,100 @@ namespace PolyhedronClass
 {
     public class Body
     {
+        #region // const
+        int[,] HexaedrIndex =
+        {   {3,2,1,0},
+            {4,5,6,7},
+            {5,1,2,6},
+            {6,2,3,7},
+            {3,0,4,7},
+            {0,1,5,4} };
+        int[,] OctaedrIndex =
+        {   {4,1,0},
+            {4,2,1},
+            {4,3,2},
+            {4,0,3},
+            {5,0,1},
+            {5,1,2},
+            {5,2,3},
+            {5,3,0} };
+        static double IcoX = 0.525731112119133606;
+        static double IcoZ = 0.850650808352039932;
+        double[,] IcoData =
+        {   {-IcoX,     0,  IcoZ},
+            { IcoX,     0,  IcoZ},
+            {-IcoX,     0, -IcoZ},
+            { IcoX,     0, -IcoZ},
+            {    0,  IcoZ,  IcoX},
+            {    0,  IcoZ, -IcoX},
+            {    0, -IcoZ,  IcoX},
+            {    0, -IcoZ, -IcoX},
+            { IcoZ,  IcoX,     0},
+            {-IcoZ,  IcoX,     0},
+            { IcoZ, -IcoX,     0},
+            {-IcoZ, -IcoX,     0} };
+
+        int[,] IcoIndex =
+        {   { 0,  4,  1},
+            { 0,  9,  4},
+            { 9,  5,  4},
+            { 4,  5,  8},
+            { 4,  8,  1},
+            { 8, 10,  1},
+            { 8,  3, 10},
+            { 5,  3,  8},
+            { 5,  2,  3},
+            { 2,  7,  3},
+            { 7, 10,  3},
+            { 7,  6, 10},
+            { 7, 11,  6},
+            {11,  0,  6},
+            { 0,  1,  6},
+            { 6,  1, 10},
+            { 9,  0, 11},
+            { 9, 11,  2},
+            { 9,  2,  5},
+            { 7,  2, 11} };
+        int[,] DodecIndex =
+        {   { 0,  1,  9, 16,  5},
+            { 1,  0,  3, 18,  7},
+            { 1,  7, 11, 10,  9},
+            {11,  7, 18, 19,  6},
+            { 8, 17, 16,  9, 10},
+            { 2, 14, 15,  6, 19},
+            { 2, 13, 12,  4, 14},
+            { 2, 19, 18,  3, 13},
+            { 3,  0,  5, 12, 13},
+            { 6, 15,  8, 10, 11},
+            { 4, 17,  8, 15, 14},
+            { 4, 12,  5, 16, 17} };
+        #endregion
+        public double SizeBody { get; set; } = 0.3;
+        public double[][] Vertexs { get; set; } //массив вершин всех тел
+        public double[][] VertexsT { get; set; } //массив вершин всех тел
+        public Edge[] Edges; //массив ребер всех тел
+        public Face[] Faces; //массив граней всех тел
+        double[,] dodec = new double[20, 3];
+        public Body(byte fl)
+        {
+            switch (fl)
+            {
+                case 0:
+                    Tetraedr(SizeBody);
+                    break;
+                case 1:
+                    Hexaedr(SizeBody);
+                    break;
+                case 2:
+                    Octaedr(SizeBody);
+                    break;
+                case 3:
+                    Icosahedron(SizeBody);
+                    break;
+                case 4:
+                    Dodecahedron(SizeBody);
+                    break;
+            }
+        }
     }
 }
